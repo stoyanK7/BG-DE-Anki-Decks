447ad887fbfd8daa7fceddb946ca7e2820b621e8
Stoyan Kostadinov <stoyank127@gmail.com>
2024-04-19 18:39:16 +0200

    add universal logger

---
+++ src/utils/logger.py
@@ -0,0 +1,24 @@
+"""Universal logger for all scripts."""
+
+import logging.config
+
+config = {
+    'version': 1,
+    'formatters': {
+        'simple': {
+            'format': '[%(levelname)s | %(module)s | %(lineno)s]: %(message)s'
+        }
+    },
+    'handlers': {
+        'stdout': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            'stream': 'ext://sys.stdout',
+        }
+    },
+    'loggers': {'root': {'level': 'INFO', 'handlers': ['stdout']}},
+}
+
+logging.config.dictConfig(config=config)
+
+logger = logging.getLogger('universal_logger')
