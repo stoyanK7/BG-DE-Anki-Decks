9d2fd74f2598fff28c1d2e75fd67149a4275cfd9
Stoyan Kostadinov <stoyank127@gmail.com>
2024-05-25 19:04:44 +0200

    improve readability

--- src/convert_pdf_to_txt.py
+++ src/convert_pdf_to_txt.py
@@ -1,36 +1,45 @@
 """Convert the wordlist PDF file to TXT file.
 
+Reason for conversion is so that the TXT file can later be parsed
+and words and translations can be extracted.
+
 Note: This script was used only once during the beginning of the project.
-The exported .TXT file has since been manually edited to fix parsing
-errors. See the following commits for the manual changes:
- - ae73703527e53d52634e05f85625258605e19761
- - 811d2298acb3ac9a466143ed2562a31faaff3054
- - 6fc67be7d450372be5bbe4dcf249ffef39c4c93e
+The exported TXT file has since been manually edited to fix parsing
+errors.
 """
 
 import os
 import subprocess
-import sys
 
 from utils.constants import WORDLIST_PDF_PATH, WORDLIST_TXT_PATH
 from utils.logger import logger
 
 if not os.path.exists(WORDLIST_PDF_PATH):
+    logger.error('PDF file not found')
     logger.error(f'{WORDLIST_PDF_PATH} does not exist')
-    sys.exit()
+    raise SystemExit('Aborting')
 
-wordlist_first_page = '16'
-wordlist_last_page = '102'
+completed_process = subprocess.run(
+    [
+        'pdftotext',
+        '-layout',  # Maintains the original physical layout of the file.
+        '-f',  # Specifies the first page to convert.
+        '16',
+        '-l',  # Specifies the last page to convert.
+        '102',
+        WORDLIST_PDF_PATH,
+        WORDLIST_TXT_PATH,
+    ],
+    capture_output=True,
+)
 
-command = [
-    'pdftotext',
-    '-layout',  # Maintain the original physical layout of the text.
-    '-f',  # Specifies the first page to convert.
-    wordlist_first_page,
-    '-l',  # Specifies the last page to convert.
-    wordlist_last_page,
-    WORDLIST_PDF_PATH,
-    WORDLIST_TXT_PATH,
-]
-subprocess.run(command, stdout=subprocess.PIPE)
+if completed_process.returncode != 0:
+    logger.error(
+        f'pdftotext returned non-zero exit status '
+        f'{completed_process.returncode}'
+    )
+    logger.error(completed_process.stderr)
+    raise SystemExit('Aborting')
+
+logger.info('Converted PDF to TXT')
 logger.info(f'Saved TXT at {WORDLIST_TXT_PATH}')
